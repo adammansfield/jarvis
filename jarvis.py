724d2562493ddee86e98adcd4b46df9112ba782c
Adam Mansfield <adam@adammansfield.org>
2011-08-29 17:10:29 -0400

    added get weather using pywapi

--- jarvis.py
+++ jarvis.py
@@ -1,40 +1,42 @@
 import win32com.client
-import time
+import pywapi
 import random
-
+import string
+import time
 
 first_name = 'Adam'
 last_name = 'Mansfield'
-
+city = 'hamilton'
+location_id = 'CAXX0184'
 
 def get_greeting():
     option = random.uniform(0,100)
-    if   (option<=25): greetingName = ', Mr. ' + last_name + '. '
-    elif (option<=50): greetingName = ' ' + first_name + '. '
-    elif (option<=75): greetingName = ' sir. '
-    else:              greetingName = '. '
+    if   (option<=25): greeting_name = ', Mr. ' + last_name + '. '
+    elif (option<=50): greeting_name = ' ' + first_name + '. '
+    elif (option<=75): greeting_name = ' sir. '
+    else:              greeting_name = '. '
 
     hour = int(time.strftime("%H", time.localtime()))
     option = random.uniform(0,100)
-    if   (option<=20):              greeting = 'Hello'          + greetingName
-    elif (option<=40):              greeting = 'Hi'             + greetingName
-    elif (hour >=0 and hour < 5):   greeting = 'Good night'     + greetingName
-    elif (hour >=5 and hour < 12):  greeting = 'Good morning'   + greetingName
-    elif (hour >=12 and hour < 17): greeting = 'Good afternoon' + greetingName
-    else:                           greeting = 'Good evening'   + greetingName
+    if   (option<=20):              greeting = 'Hello'          + greeting_name
+    elif (option<=40):              greeting = 'Hi'             + greeting_name
+    elif (hour >=0 and hour < 5):   greeting = 'Good night'     + greeting_name
+    elif (hour >=5 and hour < 12):  greeting = 'Good morning'   + greeting_name
+    elif (hour >=12 and hour < 17): greeting = 'Good afternoon' + greeting_name
+    else:                           greeting = 'Good evening'   + greeting_name
     return greeting
 
 def get_signoff():
     option = random.uniform(0,100)
-    if   (option<=33): signoffName = ' ' + first_name + '. '
-    elif (option<=66): signoffName = ' sir. '
-    else:              signoffName = '. '
+    if   (option<=33): signoff_name = ' ' + first_name + '. '
+    elif (option<=66): signoff_name = ' sir. '
+    else:              signoff_name = '. '
 
     hour = int(time.strftime("%H", time.localtime()))
-    if   (hour >=0  and hour < 5):  signoff = 'That is all for tonight. Have a pleasant sleep'          + signoffName
-    elif (hour >=5  and hour < 12): signoff = 'That is all for this morning. Have a pleasant day'       + signoffName
-    elif (hour >=12 and hour < 17): signoff = 'That is all for this afternoon. Have a pleasant evening' + signoffName
-    else:                           signoff = 'That is all for this evening. Have a pleasant night'     + signoffName
+    if   (hour >=0  and hour < 5):  signoff = 'That is all for tonight. Have a pleasant sleep'          + signoff_name
+    elif (hour >=5  and hour < 12): signoff = 'That is all for this morning. Have a pleasant day'       + signoff_name
+    elif (hour >=12 and hour < 17): signoff = 'That is all for this afternoon. Have a pleasant evening' + signoff_name
+    else:                           signoff = 'That is all for this evening. Have a pleasant night'     + signoff_name
     return signoff
 
 def get_date():
@@ -42,9 +44,18 @@ def get_date():
     long_date = time.strftime("%A, %B %d", time.localtime())
     return "It's " + short_time + ' on ' + long_date + ". "
 
+def get_weather():
+    weather = pywapi.get_weather_from_yahoo(location_id, 'metric')
+    currentCondition = string.lower(weather['condition']['text'])
+    currentTemp = weather['condition']['temp']
+    highTemp = weather['forecasts'][0]['high']
+    lowTemp = weather['forecasts'][0]['low']
+    return 'The weather in ' + city + ' is currently ' + currentCondition + ' with a temperature of ' + currentTemp + ' degrees. '\
+           'Today there will be a high of ' + highTemp + ', and a low of ' + lowTemp + '. '
+
 def main():
     voice = win32com.client.Dispatch("SAPI.SpVoice")
-    text = get_greeting() + get_date() + get_signoff()
+    text = get_greeting() + get_date() + get_weather() + get_signoff()
     print text
     voice.Speak(text)
 
