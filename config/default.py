26e47beb57fd23694de341c71f6bcbe7fd9f67b8
Adam Mansfield <adam@adammansfield.org>
2011-09-06 20:59:52 -0400

    removed some unnecssary instructions

--- config/default.py
+++ config/default.py
@@ -2,11 +2,6 @@
 Settings
   * Default file: if ./config/userconfig.py does not exist this will be copied to be used.
   * Type in your details here to personalize sayings to you. 
-  * To get your location id for weather 
-      - Open this URL: http://www.weather.gov/xml/current_obs/seek.php?state=az&Find=Find
-      - Select the necessary state state. Click 'Find'.
-      - Find the necessary station in the 'Observation Location' column.
-      - The station ID is in the URL for the weather page for that station.
 '''
 
 first_name = 'John'
@@ -15,3 +10,4 @@ city = 'Hamilton'
 country = 'Canada'
 email_username = 'default@gmail.com'
 email_password = '1234'
+
